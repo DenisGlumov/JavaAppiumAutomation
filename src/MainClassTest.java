81f5b8588c442def508fb5a28a20b8274473ba9e
glumovdenis <den.glumov@gmail.com>
2023-04-22 23:04:00 +0700

    HomeWork1 test3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,8 +9,14 @@ public class MainClassTest {
     public void testGetLocalNumber() {
         Assert.assertTrue("тест не пройден, возвращаемое число не равно 14", mainClass.getLocalNumber() == 14);
     }
+
     @Test
     public void testGetClassNumber() {
-        Assert.assertTrue("число меньше чем 45",mainClass.getClassNumber()>45);
+        Assert.assertTrue("число меньше чем 45", mainClass.getClassNumber() > 45);
+    }
+
+    @Test
+    public void testGetClassString() {
+        Assert.assertTrue("неверная строка", mainClass.getClassString().contains("hello") || mainClass.getClassString().contains("Hello"));
     }
 }
